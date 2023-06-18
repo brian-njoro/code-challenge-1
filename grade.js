cbf71b4cab597ed314464924812f4b873b428b94
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-06-18 17:00:21 +0300

    add operation for approved speed below 70

--- grade.js
+++ grade.js
@@ -20,12 +20,14 @@ function generateGrades(marks) {
     } else if (marks >= 60 && marks <=79 ) {
         return `your grade is B`
     } else if (marks >= 49 && marks <=59 ) {
-        return `Your grade is C`
+        return (`Your grade is C`)
     } else if (marks >= 40 && marks <=49){
         return `Your grade is D`
     } else if (marks < 40 && marks >= 0){
         return `Your grade is E`
     }
+    return console.log(generateGrades(marks))
+    
 }
 
 //test for function
